4b22ce0c7b267aaf420ec084198951bc87f7df0c
pavelnikitin <pavelnikitin@>
2025-06-21 17:12:29 +0400

    added full_code_with_comments

---
+++ src/test/java/softAssertionsTest.java
@@ -0,0 +1,52 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class softAssertionsTest {
+
+    @BeforeAll
+    static void basicSettingBrowder() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void softAssertionGithubTest() {
+        // открыть страницу github
+        open("https://github.com");
+        // нажать на поиск, ввести в поле воода selenide и нажать enter
+        $(".header-search-button").click();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        // нажать на selenide/selenide == перый разультат поиска
+        // нажать на первый результат поиска
+        $("span.search-match").click();
+        // перейдите в раздел Wiki проекта
+        $(byText("Wiki")).click();
+        // убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-wrapper").shouldHave(text("Soft assertions"));
+        // откройте страницу SoftAssertions
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        // скроллим до нужного нам заголовка с 3. Using JUnit5 extend test class:
+        $("[id='user-content-3-using-junit5-extend-test-class']")
+                .scrollIntoView(true)
+                .shouldBe(visible);
+        // проверьте что внутри есть ПРИМЕР кода для JUnit5
+        $("#gollum-markdown-content").shouldHave(
+                text("Using JUnit5 extend test class"),
+                text("@ExtendWith({SoftAssertsExtension.class})"),
+                text("class Tests {"),
+                text("    @Test"),
+                text("    void test() {"),
+                text("        Configuration.assertionMode = SOFT;"),
+                text("        open(\"page.html\");"),
+                text("        $(\"#first\").should(visible).click();"),
+                text("        $(\"#second\").should(visible).click();")
+                );
+        sleep(4000);
+    }
+}
